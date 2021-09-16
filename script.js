d8b5bccfddfc269f1c45188f34b7d889c6b82ab8
Kichul Kang <kckang1103@gmail.com>
2021-09-16 12:07:51 -0400

    intial commit with 3 files

---
+++ script.js
@@ -0,0 +1,8 @@
+let letVar = "let";
+var varVar = "var";
+const constVar = "const";
+
+console.log(letVar, varVar, constVar);
+
+const actors = ["Daniel Radcliffe", "Emma Watso", "Rupert Grint"]
+console.log(actors)
