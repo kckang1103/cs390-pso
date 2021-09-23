b7c314c7ddbb03372ed577fb775665b8049517a1
Kichul Kang <kckang1103@gmail.com>
2021-09-23 11:35:23 -0400

    hello

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ const constVar = "const";
 console.log(letVar, varVar, constVar);
 
 const actors = ["Daniel Radcliffe", "Emma Watso", "Rupert Grint"]
-console.log(actors)
+console.log(actors);
+
+actors.forEach(actor => console.log(actor));
+let newActors = actors.map(actor => "hello " + actor);
+console.log(newActors);
