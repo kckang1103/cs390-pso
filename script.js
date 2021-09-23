fd53cc5b589ef45232efa15abb7e1139acff4245
Kichul Kang <kckang1103@gmail.com>
2021-09-23 11:52:15 -0400

    pso 4

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let letVar = "let";
-var varVar = "var";
-const constVar = "const";
+let header = document.getElementById("heading1");
+header.innerHTML = "changed heading1";
 
-console.log(letVar, varVar, constVar);
+header.style.backgroundColor = "red";
 
-const actors = ["Daniel Radcliffe", "Emma Watso", "Rupert Grint"]
-console.log(actors);
+let body = document.getElementsByTagName("body")[0];
+let h3 = document.createElement("h3");
+h3.innerHTML = "heading 3";
+body.appendChild(h3);
 
-actors.forEach(actor => console.log(actor));
-let newActors = actors.map(actor => "hello " + actor);
-console.log(newActors);
+let clickHandler = () => {
+    header.innerHTML = "new new heading";
+    header.style.backgroundColor = "green";
+}
+
+let submitHandler = (event) => {
+    event.preventDefault();
+    let input = document.getElementById("form").value;
+    let h4 = document.createElement("h4");
+    h4.innerHTML = input;
+    body.appendChild(h4);
+    alert(input)
+    document.getElementById("form").value = "";
+}
